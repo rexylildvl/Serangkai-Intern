409115f8ec884ca929806693ec650f1e74eda9c7
rexylildvl <raiadignaamanda@gmail.com>
2025-08-15 09:33:30 +0700

    listing faq beranda

--- resources/views/pages/beranda.blade.php
+++ resources/views/pages/beranda.blade.php
@@ -448,8 +448,8 @@ use Illuminate\Support\Facades\Storage;
                                 <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                             </svg>
                         </button>
-                        <div x-show="open" x-collapse class="px-6 pb-4 text-[#626F47] bg-[#F5FBE7] **list-decimal**">
-                            {{ $faq->jawaban }}
+                        <div x-show="open" x-collapse class="prose faq-content px-6 pb-4 text-[#626F47] bg-[#F5FBE7]">
+                            {!! $faq->jawaban !!}
                         </div>
                     </div>
                     @empty
