4be8a8b4e52954ccefec92ebc3b4e1b77bbcf20d
rexylildvl <raiadignaamanda@gmail.com>
2025-08-19 10:04:31 +0700

    kelarrr

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,7 @@
+<tr>
+<td class="header">
+    <a href="{{ $url }}" style="display: inline-block;">
+        <img src="{{ asset('images/logo-ts.png') }}" class="logo" alt="Logo" height="60">
+    </a>
+</td>
+</tr>
